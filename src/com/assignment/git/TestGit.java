d325ca66c95c81b71833e63c2ea2a4353ae389ae
Alina Guta <alina_guta@yahoo.com>
2020-03-13 18:07:42 +0200

    First commit with TestGit class

---
+++ src/com/assignment/git/TestGit.java
@@ -0,0 +1,10 @@
+package com.assignment.git;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git! The name of the license "
+				+ "project is Supervised Classification Algorithms");
+	}
+
+}
