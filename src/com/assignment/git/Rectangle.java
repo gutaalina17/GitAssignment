b3ff7be91607013d2722ec9b1c2b6abcdde8dcc6
Alina Guta <alina_guta@yahoo.com>
2020-03-13 18:10:23 +0200

    Second commit with Rectangle class

---
+++ src/com/assignment/git/Rectangle.java
@@ -0,0 +1,14 @@
+package com.assignment.git;
+
+public class Rectangle {
+	private float length;
+	private float height;
+	
+	public Rectangle(float length, float height) {
+		this.length = length;
+		this.height = height;
+	}
+	
+	
+
+}
